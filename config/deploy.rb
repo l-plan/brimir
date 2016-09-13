3d6b475bc821d46abff034653173a95809f7ed2e
rolf <rolf@l-plan.nl>
2016-09-13 11:47:04 +0200

    add linked dir data

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,7 +44,7 @@ set :log_level, :debug
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_dirs, fetch(:linked_dirs, []).push('log','tmp/pids','tmp/cache','tmp/sockets','vendor/bundle','public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log','tmp/pids','tmp/cache','tmp/sockets','vendor/bundle','public/system', 'data')
 
 
 # Default value for default_env is {}
